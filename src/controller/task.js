44dbd7a9d04f271c17de429825233dceae053c2a
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-11-07 09:07:36 +0200

    added PUT

--- src/controller/task.js
+++ src/controller/task.js
@@ -2,19 +2,6 @@ import { v4 as uuidv4 } from "uuid";
 
 let tasks = [];
 
-const GET_RESPONSE = (req, res) => {
-  res.status(200).json({ response: "heyooooo" });
-};
-
-const GET_HEAD_OR_TAILS = (req, res) => {
-  const response = Math.random() > 0.5 ? "Heads" : "Tails";
-  res.status(200).json({ response: response });
-};
-
-const GENERATE_ID = (req, res) => {
-  return res.status(200).json({ id: uuidv4() });
-};
-
 const INSERT_TASK = (req, res) => {
   const task = {
     id: uuidv4(),
@@ -56,6 +43,20 @@ const GET_TASK_BY_ID = (req, res) => {
   return res.status(200).json({ task: task });
 };
 
+const UPDATE_TASK_BY_ID = (req, res) => {
+  const task = tasks.find((t) => t.id === req.params.id);
+
+  if (!task) {
+    return res.status(404).json({ message: "tasks not exist" });
+  }
+
+  const index = tasks.findIndex((t) => t.id === req.params.id);
+
+  tasks[index] = { ...tasks[index], ...req.body };
+
+  return res.status(200).json({ message: "Task was updated" });
+};
+
 const DELETE_TASK_BY_ID = (req, res) => {
   const task = tasks.find((t) => t.id === req.params.id);
 
@@ -72,11 +73,9 @@ const DELETE_TASK_BY_ID = (req, res) => {
 };
 
 export {
-  GET_RESPONSE,
-  GET_HEAD_OR_TAILS,
-  GENERATE_ID,
   INSERT_TASK,
   GET_ALL_TASKS,
   GET_TASK_BY_ID,
   DELETE_TASK_BY_ID,
+  UPDATE_TASK_BY_ID,
 };
--- src/route/task.js
+++ src/route/task.js
@@ -1,26 +1,19 @@
 import express from "express";
 
 import {
-  // GET_RESPONSE,
-  // GET_HEAD_OR_TAILS,
-  // GENERATE_ID,
   INSERT_TASK,
   GET_ALL_TASKS,
   GET_TASK_BY_ID,
+  UPDATE_TASK_BY_ID,
   DELETE_TASK_BY_ID,
 } from "../controller/task.js";
 
 const router = express.Router();
 
-// router.get("/getResponse", GET_RESPONSE);
-
-// router.get("/getHeadsOrTails", GET_HEAD_OR_TAILS);
-
-// router.get("/generateId", GENERATE_ID);
-
 router.post("/tasks", INSERT_TASK);
 router.get("/tasks", GET_ALL_TASKS);
 router.get("/tasks/:id", GET_TASK_BY_ID);
+router.put("/tasks/:id", UPDATE_TASK_BY_ID);
 router.delete("/tasks/:id", DELETE_TASK_BY_ID);
 
 export default router;
